2e454cc19d4c46478beadc98404fe288a64eec2b
BrunoGzr <Bruno83375@gmail.com>
2026-01-30 02:56:35 -0300

    Added the Player, and spawned to the screen.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,12 +9,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player_x_initial = SCREEN_WIDTH / 2
+    player_y_initial = SCREEN_HEIGHT / 2
+    p = Player(player_x_initial, player_y_initial)
     while True:
         log_state()
         screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        p.draw(screen)
+        
         pygame.display.flip()
     
     
