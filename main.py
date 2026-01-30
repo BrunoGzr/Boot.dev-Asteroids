11b0374a5a735d1f127ee86987fdf3c08ec074e5
BrunoGzr <Bruno83375@gmail.com>
2026-01-30 04:06:37 -0300

    Asteroids spawns added

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroid_field import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player_x_initial = SCREEN_WIDTH / 2
-    player_y_initial = SCREEN_HEIGHT / 2
-    p = Player(player_x_initial, player_y_initial)
+    AsteroidField.containers = (updatable)
+    field = AsteroidField()
+    asteroids = pygame.sprite.Group()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    Asteroid.containers = (asteroids, updatable, drawable)
     while True:
         log_state()
         screen.fill("black")
@@ -22,9 +29,9 @@ def main():
                 return
         clock.tick(60)
         dt = clock.tick(60) / 1000
-        p.draw(screen)
-        p.update(dt)
-        
+        updatable.update(dt)
+        for draws in drawable:
+            draws.draw(screen)
         pygame.display.flip()
     
     
